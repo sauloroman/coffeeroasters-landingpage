efe6e7544d2367f29f9147144ba7d314d6dfb6b6
Saulo Roman Santillan Nava <romansantillan1998@outlook.com>
2024-08-04 16:18:11 -0600

    Summary section mobile done

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,12 +5,11 @@ const navigation = document.querySelector('.navigation');
 const buttonOpenNav = document.querySelector('.header__icon--menu');
 const buttonCloseNav = document.querySelector('.header__icon--close');
 const questionsContainer = document.querySelector('.questions__container');
+const summaryText = document.querySelector('.summary__text');
 
 // ########################################
 // VARIABLES
 // ########################################
-
-const questionsAnswers = {}
 
 // ########################################
 // FUNCTIONS
@@ -25,7 +24,7 @@ const createOptions = function( options = [] ) {
   
   options.forEach( ({title, text}, i) => {
     questionHTML += `
-      <div class="question__option ${ i === 0 && 'question__option--selected'}">
+      <div class="question__option ${ i === 0 ? 'question__option--selected' : ''}">
         <h3 class="heading-4 question__option-title">${title}</h3>
         <p class="question__option-text">${text}</p>
       </div>
@@ -58,6 +57,14 @@ const displayQuestions = async function() {
   });
 }
 
+const displaySummary = function( summary = {} ) {
+
+  summaryText.innerHTML = `
+    &quot; I drink my coffee as <span class="summary__text-span">${summary['question-1']}</span>, with a <span class="summary__text-span">${summary['question-2']}</span> type of bean. <span class="summary__text-span">${summary['question-3']}</span> ground ala <span class="summary__text-span">${summary['question-4']}</span>, sent to me <span class="summary__text-span">${summary['question-5']}</span>&quot;
+  `;
+
+}
+
 const init = async function() {
   await handleQuestions();
 }
@@ -71,10 +78,13 @@ buttonCloseNav.addEventListener('click', toggleMenu );
 
 const handleQuestions = async function() {
 
+  const questionsAnswers = {}
+
   await displayQuestions();
 
   document.querySelectorAll('.question__option--selected').forEach( (selectedOption, i) => {
     questionsAnswers[`question-${i + 1}`] = selectedOption.firstElementChild.textContent;
+    displaySummary( questionsAnswers );
   })
   
   document.querySelectorAll('.question__icon').forEach( function( icon ) {
@@ -94,9 +104,17 @@ const handleQuestions = async function() {
   document.querySelectorAll('.question__options').forEach( function( questionOptions ) {
     questionOptions.addEventListener('click', function(e) {
       const optionClicked = e.target.closest('.question__option')
+      const questionClicked = e.target.closest('.question');
+
+      questionsAnswers[`question-${questionClicked.dataset.id}`] = optionClicked.firstElementChild.textContent;
+
       const optionsContainer = Array.from(e.target.closest('.question__options').children);
+
       optionsContainer.forEach( option => option.classList.remove('question__option--selected') );
+
       optionClicked.classList.add('question__option--selected')
+
+      displaySummary( questionsAnswers );
     });
   });
 }
